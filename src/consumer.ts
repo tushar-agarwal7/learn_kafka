d3c280d10d52f03f3f6d623d1c246af568bd4c05
tushar-agarwal7 <tushar.agarwal7373@gmail.com>
2024-07-12 22:10:27 +0530

    learn kafka

---
+++ src/consumer.ts
@@ -0,0 +1,30 @@
+import { Kafka } from "kafkajs";
+
+
+const kafka = new Kafka({
+  clientId: 'my-app',
+  brokers: ['localhost:9092']
+})
+
+
+const consumer = kafka.consumer({ groupId: 'my-app3' })
+
+const run = async () => {
+
+  
+
+    await consumer.connect()
+    await consumer.subscribe({ topic: 'payment-done', fromBeginning: true })
+  
+    await consumer.run({
+      eachMessage: async ({ topic, partition, message }) => {
+        console.log({
+          partition,
+          offset: message.offset,
+          value: message.value.toString(),
+        })
+      },
+    })}
+
+
+    run().catch(console.error)
